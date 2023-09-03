c43672681bbf28c7699103d7d096a8ed76a22dc4
Sean Butler <seanxbutler@gmail.com>
2023-09-03 12:03:27 -0700

    images can be loaded
    images can be loaded through the cli
    
    background checker board image is generated
    
    adding lena.png image file for testing

---
+++ CheckerBoardImage.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+class CheckerBoardImage
+{
+  public:
+    CheckerBoardImage();
+    CheckerBoardImage(uint32_t pixel_width, uint32_t pixel_height, uint32_t checker_tile_repeat);
+    ~CheckerBoardImage() = default;
+
+    void Generate(uint32_t pixel_width, uint32_t pixel_height, uint32_t repeat_checkers_per_line);
+    const std::vector<uint8_t> & GetImage();
+
+  private:
+    std::vector<uint8_t> image;
+    uint32_t pixelWidth = 800;
+    uint32_t pixelHeight = 600;
+    static constexpr uint32_t bytesPerPixel = 4;
+    static constexpr uint32_t defaultCheckerTileRepeat = 20;
+
+    void SetPixelColor(uint32_t x, uint32_t y, const uint8_t rgb[3]);
+};
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string_view>
 
 #include "imgui.h"
@@ -12,12 +11,14 @@
 
 #include <CLI/CLI.hpp>
 
+#include "CheckerBoardImage.h"
+
 int main(int argc, char*argv[])
 {
   constexpr std::string_view window_name = "DipTool";
 
   // setup command line arguments
-  CLI::App app{"digital image processing tool"};
+  CLI::App app{"digital bg_image processing tool"};
 
   uint32_t window_width = 800;
   app.add_option("--iw", window_width, "set window width");
@@ -25,12 +26,70 @@ int main(int argc, char*argv[])
   uint32_t window_height = 600;
   app.add_option("--ih", window_height, "set window height");
 
-  CLI11_PARSE(app, argc, argv);
+  uint32_t bg_repeat_tiles = 20;
+  app.add_option("--tr", bg_repeat_tiles, "set background checker tile repeat");
+
+  std::string image_file_path;
+  app.add_option("-i,--bg_image", image_file_path, "load bg_image to process");
+
+  CLI11_PARSE(app, argc, argv)
+
+  // setup logger
+  spdlog::enable_backtrace(32);
+
+  // load image file and checker board if image is not found
+  CheckerBoardImage checker_image(window_width, window_height, bg_repeat_tiles);
+  sf::Image loaded_image;
+
+  constexpr uint32_t loaded_image_margin = 16;
+
+  if (!image_file_path.empty())
+  {
+    spdlog::info("Trying to load: {}...", image_file_path);
+
+    bool found_image = loaded_image.loadFromFile(image_file_path);
+    if (!found_image)
+    {
+      spdlog::warn("Unable to find image file: {}", image_file_path);
+    }
+    else
+    {
+      // make the window twice as large of the original image and create pixels (margin) for some space
+      // between the original and processed image
+      window_width = (loaded_image.getSize().x * 2) + loaded_image_margin;
+      window_height = loaded_image.getSize().y + loaded_image_margin;
+
+      checker_image.Generate(window_width, window_height, bg_repeat_tiles);
+
+      spdlog::info("loaded!");
+    }
+  }
 
   // setup sfml window
   spdlog::info("initializing window...");
 
   sf::RenderWindow window(sf::VideoMode({window_width, window_height}), window_name.data());
+
+  // setup sprite
+  sf::Image bg_image;
+  bg_image.create(window_width, window_height, checker_image.GetImage().data());
+
+  sf::Texture bg_texture;
+  bg_texture.loadFromImage(bg_image);
+
+  sf::RectangleShape bg_image_plane(sf::Vector2f((float)window_width, (float)window_height));
+  bg_image_plane.setTexture(&bg_texture);
+
+  sf::Texture loaded_texture;
+  sf::Sprite loaded_image_plane;
+
+  if (loaded_image.getSize() != sf::Vector2u(0.0f,0.0f))
+  {
+    spdlog::info("setup loaded image sprite...");
+    loaded_texture.loadFromImage(loaded_image);
+    loaded_image_plane.setTexture(loaded_texture);
+    loaded_image_plane.setPosition(loaded_image_margin / 2.0f, loaded_image_margin / 2.0f);
+  }
 
   // setup imgui
   spdlog::info("initializing ui...");
@@ -38,8 +97,8 @@ int main(int argc, char*argv[])
   bool imgui_initialized = ImGui::SFML::Init(window);
   if (!imgui_initialized)
   {
-    spdlog::error("failed to initialize ImGui!");
-    return 0;
+    spdlog::critical("failed to initialize ImGui!");
+    abort();
   }
 
   // application loop
@@ -49,7 +108,7 @@ int main(int argc, char*argv[])
 
   while (window.isOpen())
   {
-    for (sf::Event event; window.pollEvent(event);)
+    for (sf::Event event{}; window.pollEvent(event);)
     {
       ImGui::SFML::ProcessEvent(window, event);
 
@@ -61,11 +120,25 @@ int main(int argc, char*argv[])
 
     ImGui::SFML::Update(window, delta_clock.restart());
 
-    ImGui::Begin("Hello, world!");
-    ImGui::Button("Press Button!");
+    if (image_file_path.empty())
+    {
+      ImGui::Begin("Click Button to Load Image...");
+    }
+    else
+    {
+      ImGui::Begin(image_file_path.c_str());
+    }
+
+    ImGui::Button("Load Image");
     ImGui::End();
 
-    window.clear();
+    window.clear(sf::Color::Black);
+    window.draw(bg_image_plane);
+
+    if (loaded_image.getSize() != sf::Vector2u(0.0f,0.0f))
+    {
+      window.draw(loaded_image_plane);
+    }
 
     ImGui::SFML::Render(window);
 
